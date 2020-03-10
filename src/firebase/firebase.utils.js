c1256417725f48fd1b7c536d5d71d1b3c16a6de9
Taicheng525 <taichen525@gmail.com>
2020-03-10 15:18:27 +0800

    add sign up form

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,32 @@ const config = {
   appId: "1:398312422882:web:f764fd1cb0e539c4874dbb"
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createAt,
+        ...additionalData
+      })
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef
+}
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import SignIn from '../../components/sign-in/sign-in.component';
 
 import './sign-in-and-sign-up.styles.scss';
+import SignUp from '../../components/sign-up.component.jsx/sign-up.component';
 
 const SignInAndSignUpPage=()=>{
   return(
     <div className="sign-in-and-sign-up">
       <SignIn />
+      <SignUp />
     </div>
   )
 }
